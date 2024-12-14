6155903f9ff2bb2c3f428ee77e57b14f47c7795a
Eric Paulin <ericpaulin78055@gmail.com>
2024-12-13 21:42:46 -0800

    display changes
    format fixed for images

--- src/App.jsx
+++ src/App.jsx
@@ -21,38 +21,40 @@ function App() {
 
   return (
     <>
-    <div className="container">
-      <h1> <span className='bluey'>momo</span> generator </h1>
+      <div className="container">
+        <h1> <span className='bluey'>momo</span> generator </h1>
 
-      {!image && (
-      <Card variant='outlined'
-        sx={{ backgroundColor: '#242424', border: 'none' , color: 'white'}}
-      >
-        <CardContent>
-        <input
-          className='generateBtn'
-          type="file"
-          accept="image/*"
-          onChange={handleImageUpload}
-        />
-        </CardContent>
-      </Card>
-      )}
+        {!image && (
+          <Card variant='outlined'
+            sx={{ backgroundColor: '#242424', border: 'none', color: 'white' }}
+          >
+            <CardContent>
+              <input
+                className='generateBtn'
+                type="file"
+                accept="image/*"
+                onChange={handleImageUpload}
+              />
+            </CardContent>
+          </Card>
+        )}
 
-      {image && (
-        <Card variant='outlined'
-        sx={{ backgroundColor: '#434343', border: 'none' , color: 'white'}}
-        >
-          <CardContent>
-            <img src={image} alt="Uploaded Momo"/>
-            <input type="text" id="topText"/>
-            <input type="text" id="bottomText"/>
-            <button>
-              MEMIFY THAT BIH
-            </button>
-          </CardContent>
-        </Card>
-      )}
+        {image && (
+          <Card variant='outlined'
+            sx={{ backgroundColor: '#434343', border: 'none', color: 'white' }}
+          >
+            <CardContent>
+              <img src={image} alt="Uploaded Momo" className='memeImg' />
+              <div className='textContainer'>
+                <input type="text" id="topText" />
+                <input type="text" id="bottomText" />
+                <button>
+                 me
+                </button>
+              </div>
+            </CardContent>
+          </Card>
+        )}
 
         <div className='footer'> made by Cuddlefish © </div>
       </div>
